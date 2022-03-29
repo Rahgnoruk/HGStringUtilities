66d42c1737d539f190bbc17aab36223c1bd91427
Rahgnoruk <tono.raimond@hotmail.com>
2022-03-29 16:33:56 -0600

    String Transformations
    Normalization and ToGuid

---
+++ Runtime/StringTransformer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HyperGnosys.Core
+{
+    public static class StringTransformer
+    {
+        public static string Normalize(this string text)
+        {
+            text = text.ToLower();
+            text = text.Replace(" ", "");
+            return text;
+        }
+        public static Guid ToGuid(this string id)
+        {
+            MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
+            byte[] inputBytes = Encoding.Default.GetBytes(id);
+            byte[] hashBytes = provider.ComputeHash(inputBytes);
+            return new Guid(hashBytes);
+        }
+    }
+}
